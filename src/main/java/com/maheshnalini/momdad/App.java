9a255c254d1749df45b0f7135083ec5ffc1686f9
mahesh <buddipammu2616@gmail.com>
2022-11-11 11:37:56 +0530

    my first commit

---
+++ src/main/java/com/maheshnalini/momdad/App.java
@@ -0,0 +1,13 @@
+package com.maheshnalini.momdad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
